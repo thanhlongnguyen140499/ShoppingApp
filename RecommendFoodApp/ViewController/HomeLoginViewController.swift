7e5d04964b4cec61b4caa6e36c766e939cfc2e01
thanhlongnguyen140499 <thanhlongnguyen140499@gmail.com>
2020-08-06 23:36:47 +0700

    Add video intro to Home login

--- RecommendFoodApp/ViewController/HomeLoginViewController.swift
+++ RecommendFoodApp/ViewController/HomeLoginViewController.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import AVKit
 
 class HomeLoginViewController: UIViewController {
+    
+    var videoPlayer:AVPlayer?
+    var videoPlayerLayer:AVPlayerLayer?
+    
     @IBOutlet weak var buttonSignUp: UIButton!
     @IBOutlet weak var buttonLogin: UIButton!
     
@@ -18,10 +23,44 @@ class HomeLoginViewController: UIViewController {
         // Do any additional setup after loading the view.
         setUpElements()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // Set up the video in the background
+        setUpVideo()
+    }
+    
     func setUpElements()  {
         
         Utilities.styleFilledButton(buttonLogin)
         Utilities.styleFilledButton(buttonSignUp)
     }
-
+    func setUpVideo() {
+        
+        // Get the path to the resource in the bundle
+        let bundlePath = Bundle.main.path(forResource: "uniqlointro", ofType: "mp4")
+        
+        guard bundlePath != nil else {
+            return
+        }
+        
+        // Create a URL from it
+        
+        let url = URL(fileURLWithPath: bundlePath!)
+        
+        // Create the video player item
+        let item = AVPlayerItem(url: url)
+    
+        // Create the player
+        videoPlayer = AVPlayer(playerItem: item)
+        
+        // Create the layer
+        videoPlayerLayer = AVPlayerLayer(player: videoPlayer!)
+        
+        //Adjust the size and frame
+        videoPlayerLayer?.frame = CGRect(x: -self.view.frame.size.width*1, y: 0, width: self.view.frame.size.width*3.7, height: self.view.frame.size.height)
+        view.layer.insertSublayer(videoPlayerLayer!, at: 0)
+        
+        // Add it to the view and play it
+        videoPlayer?.playImmediately(atRate: 1)
+    }
 }
