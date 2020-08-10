596ce3ae04ac4b291b57b3c519607bc7e608fe50
thanhlongnguyen140499 <thanhlongnguyen140499@gmail.com>
2020-08-10 22:30:09 +0700

    Push to github

---
+++ RecommendFoodApp/View/CartTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CartTableViewCell.swift
+//  RecommendFoodApp
+//
+//  Created by Nguyen Thanh Long on 8/9/20.
+//  Copyright © 2020 Nguyen Thanh Long. All rights reserved.
+//
+
+import UIKit
+
+class CartTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
